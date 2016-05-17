c240b21a55457e7e5e4c053a037bd1557ee817a4
gozu66 <gozu66@gmail.com>
2016-05-17 16:54:51 +0100

    Imported terraing generation assets

---
+++ Assets/Editor/MapGeneratorEditor.cs
@@ -0,0 +1,25 @@
+﻿using UnityEditor;
+using UnityEngine;
+using System.Collections;
+
+[CustomEditor (typeof (MapGenerator))]
+public class MapGeneratorEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        MapGenerator mapGen = (MapGenerator)target;
+
+        if(DrawDefaultInspector())
+        {
+            if(mapGen.autoUpdate)
+            {
+                mapGen.drawMapInEditor();
+            }
+        }
+
+        if(GUILayout.Button("Generate"))
+        {
+            mapGen.drawMapInEditor();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Endless-Flyer
 A casual flight sim, set in and infinite environment. 
+
+Procedural terrain generation created using [Sebastian Lague](https://github.com/SebLague)'s  tutorial series [youtube](https://goo.gl/mqAjkh) | [GitHub](https://github.com/SebLague/Procedural-Landmass-Generation) 
+
