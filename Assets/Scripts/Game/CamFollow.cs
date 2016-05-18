6753f5ab98d9792ecf19c5884ef7f3a9a2c8c24d
gozu66 <gozu66@gmail.com>
2016-05-18 17:50:19 +0100

    Input settings organized for xbox pad

--- Assets/Scripts/Game/CamFollow.cs
+++ Assets/Scripts/Game/CamFollow.cs
@@ -4,7 +4,6 @@ using System.Collections;
 public class CamFollow : MonoBehaviour 
 {
 	public Transform LookPoint, movePoint;
-	public Rigidbody rBody;
 	Vector3 refV3 = Vector3.zero;
     public float dampTime;
     Transform myT;
@@ -17,14 +16,13 @@ public class CamFollow : MonoBehaviour
 	void FixedUpdate()
 	{
 		this.transform.LookAt(LookPoint.position, Vector3.up);
-		//transform.position = Vector3.SmoothDamp(transform.position, movePoint.position, ref refV3, dampTime, 3000f, Time.deltaTime);
+		myT.position = Vector3.SmoothDamp(transform.position, movePoint.position, ref refV3, dampTime);
 
-        myT.position = Vector3.Lerp(myT.position, movePoint.position, Time.deltaTime * dampTime);
 
-		//if(Input.GetKeyDown(KeyCode.R))
-			//Application.LoadLevel(0);
+		if(Input.GetKeyDown(KeyCode.R))
+			Application.LoadLevel(0);
 
-		//if(Input.GetKeyDown(KeyCode.Escape))
-			//Application.Quit();
+		if(Input.GetKeyDown(KeyCode.Escape))
+			Application.Quit();
 	}
 }
--- Assets/Scripts/Game/PlaneMovement.cs
+++ Assets/Scripts/Game/PlaneMovement.cs
@@ -7,9 +7,7 @@ public class PlaneMovement : MonoBehaviour
 	public ForceMode myForce;
 	public float rollSpeed, pitchSpeed, yawSpeed;
 	public Transform localDirection;
-	public ForceMode myForce2;
 	float speed = 1000;
-
     Rigidbody rBody;
 
     void Start()
@@ -17,17 +15,22 @@ public class PlaneMovement : MonoBehaviour
         rBody = GetComponent<Rigidbody>();
     }
 
-	void Update()
+	void FixedUpdate()
 	{
-		speed += (Input.GetAxis("Mouse ScrollWheel")*-1)*150f;
-		speed = Mathf.Clamp(speed, 500, 1500);
-
+        speed = Mathf.Clamp(speed, 500, 1500);
         rBody.AddForce((localDirection.forward * speed) * Time.deltaTime, myForce);
+        //speed += (Input.GetAxis("Mouse ScrollWheel")*-1)*150f; 
+        speed += (Input.GetAxis("Triggers1") * -1) * 150f;
+        rBody.AddRelativeTorque(pitchSpeed * Input.GetAxis("LStick1Y") * Time.deltaTime, yawSpeed * Input.GetAxis("LStick1X") * Time.deltaTime, -rollSpeed*Input.GetAxis("RStick1X") * Time.deltaTime, myForce);
 
+        print(speed);
+
+        /*
+        speed += (Input.GetAxis("Mouse ScrollWheel")*-1)*150f;
         rBody.AddRelativeTorque(0,0,(-rollSpeed*Input.GetAxis("Mouse Y") * Time.deltaTime), myForce);
         rBody.AddRelativeTorque((pitchSpeed*Input.GetAxis("Mouse X") * Time.deltaTime),0,0, myForce);
         rBody.AddRelativeTorque(0,(yawSpeed*Input.GetAxis("Horizontal")) * Time.deltaTime,0, myForce);
-
-        //print(speed);
-	}
+        */
+    }
 }
+ 
