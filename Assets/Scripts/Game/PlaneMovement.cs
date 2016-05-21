10453fc9bba8b09073b6cb068438fab096bcc804
gozu66 <gozu66@gmail.com>
2016-05-21 02:56:57 +0100

    feckless tinkering

--- Assets/Scripts/Game/PlaneMovement.cs
+++ Assets/Scripts/Game/PlaneMovement.cs
@@ -9,6 +9,7 @@ public class PlaneMovement : MonoBehaviour
 	public Transform localDirection;
 	float speed = 1000;
     Rigidbody rBody;
+    bool isCrashed;
 
     void Start()
     {
@@ -16,15 +17,24 @@ public class PlaneMovement : MonoBehaviour
     }
 
 	void FixedUpdate()
-	{
-        speed = Mathf.Clamp(speed, 500, 2000);
-        rBody.AddForce((localDirection.forward * speed) * Time.deltaTime, myForce);
+    {
+        if (!isCrashed)
+        {
+            speed = Mathf.Clamp(speed, 500, 2000);
+            rBody.AddForce((localDirection.forward * speed) * Time.deltaTime, myForce);
 
-        speed += (Input.GetAxis("Triggers1") * -1) * 150;
-        rBody.AddRelativeTorque(pitchSpeed * Input.GetAxis("LStick1Y") * Time.deltaTime, yawSpeed * Input.GetAxis("LStick1X") * Time.deltaTime, -rollSpeed*Input.GetAxis("RStick1X") * Time.deltaTime, myForce);
+            speed += (Input.GetAxis("Triggers1") * -1) * 150;
+            rBody.AddRelativeTorque(pitchSpeed * Input.GetAxis("LStick1Y") * Time.deltaTime, yawSpeed * Input.GetAxis("RStick1X") * Time.deltaTime, -rollSpeed * Input.GetAxis("LStick1X") * Time.deltaTime, myForce);
 
-        speed += Input.GetAxis("Mouse ScrollWheel") * 600;
-        rBody.AddRelativeTorque(pitchSpeed * Input.GetAxis("Vertical") * Time.deltaTime, yawSpeed * Input.GetAxis("Mouse X") * Time.deltaTime, -rollSpeed * Input.GetAxis("Horizontal") * Time.deltaTime, myForce);
+            speed += Input.GetAxis("Mouse ScrollWheel") * 600;
+            rBody.AddRelativeTorque(pitchSpeed * Input.GetAxis("Vertical") * Time.deltaTime, yawSpeed * Input.GetAxis("Mouse X") * Time.deltaTime, -rollSpeed * Input.GetAxis("Horizontal") * Time.deltaTime, myForce);
+        }
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        // Debug.Log("HIT");
+        isCrashed = true;
     }
 }
  
